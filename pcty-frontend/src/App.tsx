bd9a093846e1021b4f87b17f65df3274a58492bd
Carla Montano <cm.emprendiendo@gmail.com>
2023-07-28 19:43:02 -0500

    update image

--- pcty-frontend/src/App.tsx
+++ pcty-frontend/src/App.tsx
@@ -6,7 +6,7 @@ import EmployeeTable from './components/EmployeeTable';
 function App() {
   return (
     <div className="App container mx-auto">
-      <img src={(require("./images/logo.jpeg"))} className='w-48 py-3' alt="logo" />
+      <a href='/'><img src={(require("./images/logo.jpeg"))} className='w-40 py-3' alt="logo" /></a>
       <h1 className="text-xl my-5 underline">Employees Healthcare Benefit Cost</h1>
       <EmployeeTable />
     </div>
